2c1d9764f1b7a4ae54617103c57cdb3d504da192
Andreea Diaconescu <diaconescu@biomed.ee.ethz.ch>
2020-07-01 19:36:10 -0400

    Fixed bug wrt file copying.

--- day4/eeg/preprocess/mmn_copy_raw_eeg_data_into_analysis_folder.m
+++ day4/eeg/preprocess/mmn_copy_raw_eeg_data_into_analysis_folder.m
@@ -1,5 +1,5 @@
 function mmn_copy_raw_eeg_data_into_analysis_folder( options )
-%MMN_COPY_RAW_EEG_DATA_INTO_ANALYSIS_FOLDER Copies raw EEG data from raw directory to current 
+%MMN_COPY_RAW_EEG_DATA_INTO_ANALYSIS_FOLDER Copies raw EEG data from raw directory to current
 %analysis folder
 
 % source: where are the raw data currently
@@ -12,21 +12,25 @@ origprefix      = 'subject';
 destsubfolder   = 'eeg';
 
 % loop over conditions and subjects
-    for idCell  = options.subjects
-        id      = char(idCell);
-
-        subjsource      = fullfile(rawdatasource, ...
-             [origprefix id '*.*df']);
-        subjdestination = fullfile(rawdatadest,...
-            [destprefix id], destsubfolder);
-        
-        if ~exist(subjdestination, 'dir')
-            mkdir(subjdestination);
-        end
-        
-        copyfile(subjsource, subjdestination);
-        disp(['Copied raw EEG data for subject ' id]);
+for idCell  = options.subjects
+    id      = char(idCell);
+    
+    subjsource      = fullfile(rawdatasource, ...
+        [origprefix id '.bdf']);
+    
+    subjdestinationDir = fullfile(rawdatadest,...
+        [destprefix id], destsubfolder);
+    
+    if ~exist(subjdestinationDir, 'dir')
+        mkdir(subjdestinationDir);
     end
+    subjdestinationFile = fullfile(subjdestinationDir, ...
+        [destprefix id '.bdf']);
+    
+    
+    copyfile(subjsource, subjdestinationFile);
+    disp(['Copied raw EEG data for subject ' id]);
+end
 
 end
 
