8f8428813c3933bb179dad547054e9f5ae871198
Erin W Dickie <edickie@users.noreply.github.com>
2020-07-05 17:46:09 -0400

    now updates the lessons repo if it already exists

--- envs/setup_scinet_kcni_rstudio_env_part1.sh
+++ envs/setup_scinet_kcni_rstudio_env_part1.sh
@@ -2,10 +2,21 @@
 
 +x
 
-echo "Cloning the kcni-school-lessons repository"
-mkdir $SCRATCH/kcni-school-data
-cd $SCRATCH/kcni-school-data
-git clone --recurse-submodules https://github.com/krembilneuroinformatics/kcni-school-lessons.git
+## either clone or pull the kcni-school-lessons 
+
+LESSONS_DIR=$SCRATCH/kcni-school-data/kcni-school-lessons
+if [ -d "$LESSONS_DIR" ]; then
+  ### update lessons if they exist already ###
+  echo "Updating the kcni-school-lessons repository"
+  cd $SCRATCH/kcni-school-data/kcni-school-lessons
+  git pull --recurse-submodules
+else
+  ###  Control will jump here if $DIR does NOT exists ###
+  echo "Cloning the kcni-school-lessons repository"
+  mkdir $SCRATCH/kcni-school-data
+  cd $SCRATCH/kcni-school-data
+  git clone --recurse-submodules https://github.com/krembilneuroinformatics/kcni-school-lessons.git
+fi
 
 echo "Moving larger data into the repository"
 cp /scinet/course/ss2020/5_neuroimaging/kcni_data/day1_workdir.zip $SCRATCH/kcni-school-data/kcni-school-lessons/day1/
